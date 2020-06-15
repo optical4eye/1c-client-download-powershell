478382a09d25730ed151cd21a8560838d0e44ec8
Ermakov Ivan <63299903+optical4eye@users.noreply.github.com>
2020-06-15 22:04:23 +0300

    update

--- download_1c.ps1
+++ download_1c.ps1
@@ -1,3 +1,4 @@
+#vars
 $login = user
 $pass = pass
 $url = "https://login.1c.ru/login"
@@ -8,9 +9,7 @@ $full_client_url = "https://releases.1c.ru/version_file?nick=Platform83&ver=$ver
 $thin_client_file = "C:\download\setuptc64_$version_mutate.rar"
 $full_client_file = "C:\download\windows64full_$version_mutate.rar"
 
-
-
-
+#check version url
 $creds = Invoke-WebRequest $url -SessionVariable fb
 $form = $creds.Forms[0]
 $form.fields["username"] = $login
@@ -18,3 +17,7 @@ $form.fields["password"] = $password
 $creds = Invoke-WebRequest  -Uri ($url) -WebSession $fb -Method POST -Body $form.Fields
 $download_url_thin = (Invoke-WebRequest  -Uri $thin_client_url -WebSession $fb).Links.href | Where-Object {$_ -like '*public/file/get*'} | select -Last 1
 $download_url_full = (Invoke-WebRequest  -Uri $full_client_url -WebSession $fb).Links.href | Where-Object {$_ -like '*public/file/get*'} | select -Last 1
+
+#download files
+Invoke-WebRequest -Uri $download_url_thin -WebSession $fb -OutFile "$thin_client_file"
+Invoke-WebRequest -Uri $download_url_full -WebSession $fb -OutFile "$full_client_file"
